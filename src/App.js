68fdc59b88871cfee5ef87a77c651836efd60422
LaurinJ <laurin223@seznam.cz>
2021-09-05 11:07:05 +0200

    adding quill

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import Header from "./components/Header";
 // import Navbar from "./components/Navbar";
 import Sitebar from "./components/Sitebar";
-// import moduleName from 'react'
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
+import { QuillModules, QuillFormats } from "./helpers/quill";
 
 function App() {
   return (
@@ -13,7 +15,7 @@ function App() {
       <div className="mt-14 pt-5 px-5 w-full h-screen bg-gray-300 text-gray-700">
         <h1 className="text-2xl">Add New Product</h1>
         <div className="mt-5">
-          <form className="flex justify-between">
+          <form className="flex justify-between" enctype="multipart/form-data">
             <div className="w-3/5">
               <div class="">
                 {/* <label for="inputEmail4" className="mr-2">
@@ -70,6 +72,14 @@ function App() {
                   placeholder="Krátký popis..."
                 />
               </div>
+              <ReactQuill
+                modules={QuillModules}
+                formats={QuillFormats}
+                theme="snow"
+                value=""
+                placeholder="Dlouhý popis produktu..."
+                className="mt-4 bg-gray-100 h-96"
+              />
             </div>
 
             <div className="w-96 h-80 bg-white">
@@ -81,11 +91,33 @@ function App() {
                   <option>RPG</option>
                   <option>Strategie</option>
                 </select>
+                <div class="mt-4">
+                  <label for="inputAddress2" className="mr-3">
+                    Kod produktu:
+                  </label>
+                  <input
+                    type="number"
+                    class="w-32 p-3 bg-gray-100"
+                    id="inputAddress2"
+                    placeholder="Cena"
+                  />
+                </div>
                 <div class="mt-4 ">
                   <label class="mr-3" for="gridCheck">
                     Zobrazit:
                   </label>
                   <input class="w-5 h-5 bg-green-400 text-black" type="checkbox" id="gridCheck" />
+                </div>
+                <div className="mt-4">
+                  <h5>Obrazky</h5>
+                  <hr />
+
+                  <small>Max velikost obrázku: 1mb</small>
+                  <br />
+                  <label className="inline-block mt-2 py-1 px-2 bg-green-500">
+                    Nahrát obrázky
+                    <input type="file" multiple accept="image/" />
+                  </label>
                 </div>
               </div>
             </div>
